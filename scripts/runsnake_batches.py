dcfa0f295345a907b308adf637375b1acf221ff6
jhover <jhover@transit.cshl.edu>
2023-06-21 10:57:29 -0400

    Update.

--- scripts/runsnake_batches.py
+++ scripts/runsnake_batches.py
@@ -1,14 +1,15 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 
 import argparse
 import datetime as dt
 import logging
 import os   
+import sys
 import traceback
 
 import subprocess as sp
 
-gitpath=os.path.expanduser("~/git/cshlwork")
+gitpath = os.path.expanduser("~/git/cshlwork")
 sys.path.append(gitpath)
 
 from cshlwork.utils import *
@@ -19,52 +20,39 @@ from cshlwork.utils import *
 #  CHR="3"
 #  SAMPLE='["SRR11836547","SRR11836548"]'
 #
-#
+def get_pathinfo(filepath):
+    abspath = os.path.abspath(filepath)
+    dirpath = os.path.dirname(abspath)
+    file_name = os.path.basename(abspath)
+    (base, ext) = os.path.splitext(file_name)
+    ext = ext[1:] # remove dot
+    return (dir, base, ext)
+    
 
+def run_snake_batch(mode, samples, species, chromosome, numjobs ):
+    STATUS="/grid/gillis/home/hover/git/elzar-example/snakemake/status-sge.py"
+    proj=f"cmxci.{mode}.{species}"
+    datestr = dt.datetime.now().strftime("%Y%m%d%H%M")
+    sampstr = str(samples).replace(' ','')
+    
 
-
-
-
-
-MODE="paired"
-PROJ=f"cmxci.{MODE}"
-DATESTR = dt.datetime.now().strftime("%Y%m%d%H%M")
-STATUS="/grid/gillis/home/hover/git/elzar-example/snakemake/status-sge.py"
-
-logging.getLogger().setLevel(logging.DEBUG)
-logging.debug(f"PATH={os.environ['PATH']}")
-
-for SPEC in SPECIES.keys():
-    logging.info(f'Running on {SPEC}')
-    if FIRSTONLY:
-        logging.info(f'only running first chromosome')
-        CHRS = []
-        CHRS.append(SPECIES[SPEC][0])
-    else:
-        CHRS = SPECIES[SPEC]
-
-    for CHR in CHRS:
-        logging.info(f'    Running {SPEC} -> {CHR}')
-        cmd = [ 
-                'snakemake',
-                '--keep-going',
-                '--rerun-incomplete',
-                '--latency-wait', '10',
-                '--config',f'species={SPEC}',f'chr={CHR}',
-                '--jobs', str(NJOBS),
-                '--cluster-status', STATUS,
-                '--cluster-cancel','qdel',
-                '--cluster', f'qsub -N {PROJ} -pe threads {{threads}} -wd /grid/gillis/home/hover/work/cmxci.{MODE} -l m_mem_free={{resources.mem_mb}}M'
-              ]
-        try:
-            cmdstr = ' '.join(cmd)
-            logging.info(f'    Running command: {cmdstr}')
-            run_command(cmd)
-        except NonZeroReturnException as nzre:
-            logging.error(traceback.format_exc(None))
-
-
-
+    cmd = [ 
+            'snakemake',
+            '--keep-going',
+            '--rerun-incomplete',
+            '--latency-wait', '10',
+            '--config', f'species={species}',f'chr={chromosome}',f'sample={sampstr}', 
+            '--jobs', str(numjobs),
+            '--cluster-status', STATUS,
+            '--cluster-cancel','qdel',
+            '--cluster', f'qsub -N {proj} -pe threads {{threads}} -wd /grid/gillis/home/hover/work/cmxci.{mode} -l m_mem_free={{resources.mem_mb}}M'
+          ]
+    try:
+        cmdstr = ' '.join(cmd)
+        logging.info(f'    Running command: {cmdstr}')
+        run_command(cmd)
+    except NonZeroReturnException as nzre:
+        logging.error(traceback.format_exc(None))
 
 
 if __name__ == '__main__':
@@ -93,37 +81,32 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--species', 
                         metavar='species', 
                         type=str,
+                        required=True,
                         default=None, 
                         help='species to run on. ')
-    
+
+    parser.add_argument('-c', '--chromosome', 
+                        metavar='chromosome', 
+                        type=str,
+                        default='1', 
+                        help='chromsome ')
+
     parser.add_argument('-j','--numjobs', 
                         metavar='numjobs',
                         required=False,
-                        default=5,
+                        default=2,
                         type=int, 
                         help='Max simultaneous jobs.')
 
     parser.add_argument('-b','--batchsize', 
                         metavar='batchsize',
                         required=False,
-                        default=5,
+                        default=2,
                         type=int, 
                         help='Number of samples per batch.')
 
-   
-#    parser.add_argument('srafiles', 
-#                        metavar='srafiles', 
-#                        nargs='+',
-#                        type=argparse.FileType('r'), 
-#                        help='SRA files to process. ')
-#
-#        for f in args.srafiles:
-#               for line in f:
-#                      process...
-#   
-
-    parser.add_argument('srafiles', 
-                        metavar='srafiles', 
+    parser.add_argument('infiles', 
+                        metavar='infiles', 
                         nargs='+',
                         type=str, 
                         help='SRA files to process. ') 
@@ -138,37 +121,20 @@ if __name__ == '__main__':
     logging.debug(args)
 
 
-
-
-
-
-
-
-
-
-
-NJOBS=2
-#SPECIES="Bos_taurus"
-#CHR="1"
-
-#SPECIES="Canis_lupus_familiaris"
-
-#SPECIES="Capra_hircus"
-
-#SPECIES="Equus_caballus"
-
-
-#SPECIES="Macaca_mulatta"
-#SPECIES="Monodelphis_domestica"
-#SPECIES="Oryctolagus_cuniculus"
-#SPECIES="Sus_scrofa"
-
-
-
-PROJ="cmxci.$MODE.$SPECIES"
-DATE=`date +"%Y%m%d.%H%M"`
-
-echo 'Running on all samples...'
-echo snakemake --keep-going --config species=$SPECIES sample=$SAMPLE chr=$CHR --jobs $NJOBS --cluster "qsub -N $PROJ -pe threads {threads} -wd ~/work/cmxci.$MODE -l m_mem_free={resources.mem_mb}M" 
-
-time snakemake --keep-going --config species=$SPECIES sample=$SAMPLE chr=$CHR --jobs $NJOBS --cluster "qsub -N $PROJ -pe threads {threads} -wd ~/work/cmxci.$MODE -l m_mem_free={resources.mem_mb}M" > smlog.$DATE.txt 2>&1
+    samplelist = []
+    for filename in args.infiles:
+        (dir, base, ext) = get_pathinfo(filename)
+        samplelist.append(base)
+    
+    batchlist = [samplelist[i:i + args.batchsize] for i in range(0, len(samplelist), args.batchsize)]
+    logging.debug(f'batchlist= {batchlist}')
+    
+    batchno = 0
+    for batch in batchlist:
+        batchno += 1
+        logging.debug(f'executing w/ mode={args.mode} species={args.species} chr={args.chromosome} numjobs={args.numjobs}')
+        logging.info(f'executing batch [{batchno}/{len(batchlist)}] w/ samples={batch}')
+        run_snake_batch(args.mode, batch, args.species, args.chromosome, args.numjobs )
+        logging.info(f'finished invocation of snakemake...') 
+    
+      
